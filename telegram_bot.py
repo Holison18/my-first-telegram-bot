fdf6afc53bfd0f4a5cf0cb25020b00423e9a3992
Holison18 <kobbyholison2002@gmail.com>
2023-06-24 11:00:10 +0000

    telegram bot

--- telegram_bot.py
+++ telegram_bot.py
@@ -17,8 +17,30 @@ def help(update,context):
     /content -> View content
     /python -> A python playlist
     /javascript -> View Javascript playlist
-    /C++ -> View C++ playlist
+    /Cpp -> View C++ playlist
     /contact -> Contact Information
     """)
 
-def
+def content(update,context):
+    update.message.reply_text("We have links to various course playlist")
+
+def python(update,context):
+    update.message.reply_text("""You can get good python video tutorials on the following sites:
+    https://youtube.com/playlist?list=PLTjRvDozrdlxj5wgH4qkvwSOdHLOCx10f
+    https://youtube.com/playlist?list=PLWKjhJtqVAbnqBxcdjVGgT3uVR10bzTEB
+    https://youtube.com/playlist?list=PL9ooVrP1hQOE4KoZLUP4LgBwFH2IJCQs6
+    """)
+
+def javascript(update,context):
+    update.message.reply_text("""You can get good javascript video tutorials on the following sites:
+    https://youtube.com/playlist?list=PLsyeobzWxl7rrvgG7MLNIMSTzVCDZZcT4
+    https://youtube.com/playlist?list=PLWKjhJtqVAbleDe3_ZA8h3AO2rXar-q2V
+    """)
+
+def cpp(update,context):
+    update.message.reply_text("""You can get good javascript video tutorials on the following sites:
+    https://youtube.com/playlist?list=PL_c9BZzLwBRJVJsIfe97ey45V4LP_HXiG
+    """)
+
+def contact(update,context):
+    update.message.reply_text("Call: 0503986624 for assistance")
