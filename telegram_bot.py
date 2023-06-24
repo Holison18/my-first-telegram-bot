500a301e5fd2218d0c58c39808dd2885a7d849b1
Holison18 <kobbyholison2002@gmail.com>
2023-06-24 10:45:27 +0000

    telegram bot

--- telegram_bot.py
+++ telegram_bot.py
@@ -3,3 +3,22 @@ import telegram.ext
 token = "https://www.drawize.com/play?sscid=61k7_z9k2k&#"
 
 update =  telegram.ext.update(token,use_contexto=True)
+
+dispatcher = update.dispatcher
+
+def start(update,context):
+    update.message.reply_to("Welcome to my Bot")
+
+
+def help(update,context):
+    update.message.reply_to("""
+    /start -> Welcome to channel
+    /help  -> Ask for help
+    /content -> View content
+    /python -> A python playlist
+    /javascript -> View Javascript playlist
+    /C++ -> View C++ playlist
+    /contact -> Contact Information
+    """)
+
+def
